28644dbc48e99a75770d9e3fcb67f1092fdbd2f5
skvisual <southkvisual@gmail.com>
2020-03-18 18:45:36 -0500

    completed some functionality of Gracenote api

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-//Variables that need defining: startDate <--input type date, zipCode <--input type text, range
-
 $('#button').on('click',function() {
     getShowtimes()
 });
@@ -9,17 +7,22 @@ function getShowtimes () {
 var date = $('#date').val();
 var zipCode = $('#zipCode').val();
 var range = $('#searchRange').val();
-console.log(date)
+// console.log(date)
 var queryURLShowtimes = "http://data.tmsapi.com/v1.1/movies/showings?startDate=" + date +"&zip=" + zipCode + "&radius=" + range +"&api_key=92rsd8kpdrnkajvyvb42dkug";
 
 $.ajax({
     url: queryURLShowtimes,
     method: "GET"
 }).then(function(response) {
-    // console.log(response[0])
-    for (i = 0; i < 20; i++) {
+    // console.log(response)
+
+    for (i = 0; i < 10; i++) {
         $('#showdata').append(response[i].title + "<br>")
+        console.log(response[i].showtimes) 
     }
+
+
+
 })
 }  
 
