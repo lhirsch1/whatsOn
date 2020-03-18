8145d43cb9dc4e8aafe35256635708489d96bbd9
frunox <60527588+frunox@users.noreply.github.com>
2020-03-18 16:33:06 -0500

    update modal, try to capture data inputs

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
-if (localStorage.getItem('mod') != null){
-    alert ('yes');
-  } else {
-    console.log("Memory empty");
-    var modalTitle = $('.modal-title');
+var date = $('#date');
+var zipCode = $('#zipCode');
+var range = $('#searchRange');
+
+function openModal() {
+  var modalTitle = $('.modal-title');
     // render the error message to the modal title h5 element
     modalTitle.text('Enter Your Preferences');
-    $('.carousel').carousel({
-        interval: false
-      })
     $('#myModal').modal('show');
+}
 
-  }
+function getUserInfo(){
+  alert('in getUserInfo');
+  date = $('#date').find('input[name="dateInput"]').val();
+  zipCode = $('#zipCode').find('input[name="zipCode"]').val();
+  range = $('#range').find('input[name="range"]').val();
+}
+
+$('#getUserInfo').on('click', openModal);
+$('#nextButton').on('click', getUserInfo);
+
+// if (localStorage.getItem('mod') != null){
+//     alert ('yes');
+//   } else {
+//     console.log("Memory empty");
+//     var modalTitle = $('.modal-title');
+//     // render the error message to the modal title h5 element
+//     modalTitle.text('Enter Your Preferences');
+//     $('.carousel').carousel({
+//         interval: false
+//       })
+//     $('#myModal').modal('show');
+
+//   }
