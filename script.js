9eefaa589d82266914730e128a776b67508b4549
skvisual <southkvisual@gmail.com>
2020-03-18 18:49:53 -0500

    completed partial functionality of api integration

--- script.js
+++ script.js
@@ -18,9 +18,32 @@ $.ajax({
 
     for (i = 0; i < 10; i++) {
         $('#showdata').append(response[i].title + "<br>")
-        console.log(response[i].showtimes) 
+        // console.log(response[i].showtimes) 
+
+        for (y = 0; y <10; y++) {
+            $('#showdata').append(response[i].showtimes[])
+        }
     }
 
+    
+//build an array of theater objects. it will have to key:value pairs. theater, and showtimes.
+
+//populate 2 showtimes per theater
+
+//if theater is in array, then check if the showtimes array is less than 2. 
+
+//else if not in array, then push brand new theater to array.
+
+//if it is less than 2, we can add showtimes
+
+//else if not less than 2, then do nothing
+
+//bulid the theater objects array from 23
+
+//in another loop OUSTIDE, iterate around theater object array and append.
+
+
+    
 
 
 })
