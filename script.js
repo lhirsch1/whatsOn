caad6035cb0a7f5cebb4a4c886d26f336a11a808
Lukas Hirsch <hirschlukasa@gmail.com>
2020-03-18 17:31:33 -0500

    added comments and debugged form options

--- script.js
+++ script.js
@@ -1,91 +1,112 @@
+var format = 'tvshow';
+var title = '';
+var actor = '';
 var genreArray = [];
 var subscriptionArray = [];
+var genreContainer = $('.genreContainer');
 
 
 //function changes css of search buttons when clicked
-$('.searchBtn').on('click', function(){
-    console.log("search button")
-    if($(this).hasClass('searchBtnClicked')){
+$('.searchBtn').on('click', function () {
+    //checks if search button has been clicked already
+    if ($(this).hasClass('searchBtnClicked')) {
+        //removes clicked styling
         $(this).removeClass('searchBtnClicked');
+        //appends unclicked styling
         $(this).addClass("searchBtn");
     }
-    else{
-        $(this).addClass("searchBtnClicked");
+    else {
+        //adds clicked styling
+        $(this).addClass('searchBtnClicked');
+        //removes unclicked styling
         $(this).removeClass('searchBtn');
     }
 })
-
-$('#tvBtn').on('click', function(){
+//function is run if user wants to search TV shows
+$('#tvBtn').on('click', function () {
     var thisText = $(this).text();
-    var genreContainer = $('.genreContainer');
-    if($(this).hasClass('searchBtnClicked')){
+    //if tvBtn has already been selected
+    if ($(this).hasClass('searchBtnClicked')) {
         $('#movieBtn').removeClass('searchBtnClicked');
+        format = 'tvshow';
         genreContainer.show();
-        //if tv show show genre
-        //unselect movie button
     }
-    // else{
-    //     //hide genre search
-    //     genreContainer.hide();
-    // }
 })
 
-$('#movieBtn').on('click', function(){
+$('#movieBtn').on('click', function () {
     var thisText = $(this).text();
     var genreContainer = $('.genreContainer');
-    if($(this).hasClass('searchBtnClicked')){
+    if ($(this).hasClass('searchBtnClicked')) {
         $('#tvBtn').removeClass('searchBtnClicked');
+        format = 'movie';
+
+        //genre array is not relevant to movies and is cleared
+        genreArray = [];
+        //strips buttons of clicked styling and appends unclicked
+        $('.genreBtn').removeClass('searchBtnClicked');
+        $('.genreBtn').addClass('searchBtn');
+        //hides genre container
         genreContainer.hide();
-        //if tv show show genre
-        //unselect movie button
+
+
     }
-    else{
-        //hide genre search
-        
-    }
-})
-
-
-
-
-
-$('#movieBtn').on('click', function(){
-    var thisText = $(this).text();
-    if($(this).hasClass('searchBtnClicked')){
-    
-    }
-    else{
+    else {
         //take out of array
         var thisIndex = genreArray.indexOf(thisText);
-        genreArray.splice(thisIndex,1);
-        
+        genreArray.splice(thisIndex, 1);
+
     }
 })
+
+
+
+
+
+// $('#movieBtn').on('click', function () {
+//     var thisText = $(this).text();
+//     if ($(this).hasClass('searchBtnClicked')) {
+
+//     }
+    
+// })
 
 
 //onclick function adds selected genre to array
-$('.genreBtn').on('click', function(){
+$('.genreBtn').on('click', function () {
     var thisText = $(this).text();
-    if($(this).hasClass('searchBtnClicked')){
-    genreArray.push(thisText);
+    if ($(this).hasClass('searchBtnClicked')) {
+        genreArray.push(thisText);
     }
-    else{
+    else {
         //take out of array
         var thisIndex = genreArray.indexOf(thisText);
-        genreArray.splice(thisIndex,1);
-        
+        genreArray.splice(thisIndex, 1);
+
     }
 })
 
 
-$('.subscriptionBtn').on('click', function(){
+//handles subscription options
+$('.subscriptionBtn').on('click', function () {
     var thisText = $(this).text();
-    if($(this).hasClass('searchBtnClicked')){
-    subscriptionArray.push(thisText);
+    if ($(this).hasClass('searchBtnClicked')) {
+        //adds subscription service to array
+        subscriptionArray.push(thisText);
     }
-    else{
+    else {
         //take out of array
         var thisIndex = subscriptionArray.indexOf(thisText);
-        subscriptionArray.splice(thisIndex,1);
+        subscriptionArray.splice(thisIndex, 1);
     }
 })
+
+
+//search form submit button listener
+$('.searchForm').submit(function(e) {
+    e.preventDefault();
+    //holds user input for title and actor
+    //may have to put in lowercase for api
+    title = $('#titleInput').val();
+    actor = $('#actorInput').val();
+    console.log('format = '  + format + ' title = ' + title +  ' actor = ' + actor + ' genres = ' + genreArray + ' subscriptions = ' + subscriptionArray )
+});
