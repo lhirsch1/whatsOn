cfcc276897d09e7132b0c3ce3e35ab4db33cbea3
frunox <60527588+frunox@users.noreply.github.com>
2020-03-18 19:35:19 -0500

    remove carousel, styled modal, additional changes

--- script.js
+++ script.js
@@ -9,26 +9,13 @@ function openModal() {
     $('#myModal').modal('show');
 }
 
-function getUserInfo(){
-  alert('in getUserInfo');
+function saveUserInfo(){
+  alert('in saveUserInfo');
   date = $('#date').find('input[name="dateInput"]').val();
   zipCode = $('#zipCode').find('input[name="zipCode"]').val();
   range = $('#range').find('input[name="range"]').val();
 }
 
 $('#getUserInfo').on('click', openModal);
-$('#nextButton').on('click', getUserInfo);
+$('#saveButton').on('click', saveUserInfo);
 
-// if (localStorage.getItem('mod') != null){
-//     alert ('yes');
-//   } else {
-//     console.log("Memory empty");
-//     var modalTitle = $('.modal-title');
-//     // render the error message to the modal title h5 element
-//     modalTitle.text('Enter Your Preferences');
-//     $('.carousel').carousel({
-//         interval: false
-//       })
-//     $('#myModal').modal('show');
-
-//   }
