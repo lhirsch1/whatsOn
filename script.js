94ea90f4c313849714df59c88254b9d515d55534
Lukas Hirsch <hirschlukasa@gmail.com>
2020-03-21 10:57:54 -0500

    formating modal, filled with dummy data

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ var date = $('#date').val();
 var zipCode = $('#zipCode').val();
 var range = $('#searchRange').val();
 // console.log(date)
-var queryURLShowtimes = "http://data.tmsapi.com/v1.1/movies/showings?startDate=" + date +"&zip=" + zipCode + "&radius=" + range +"&api_key=92rsd8kpdrnkajvyvb42dkug";
+var queryURLShowtimes = "https://data.tmsapi.com/v1.1/movies/showings?startDate=" + date +"&zip=" + zipCode + "&radius=" + range +"&api_key=92rsd8kpdrnkajvyvb42dkug";
 
 $.ajax({
     url: queryURLShowtimes,
